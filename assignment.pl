5038b2b4487953c660e311555582c72a1e814b6b
FriggyStudios <friggystudios@gmail.com>
2017-11-16 20:53:34 +0000

    push

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,12 @@
 %Definitions for isElementInList(El, List)
-
-%Definitions for reverseList(List, ReversedList)
-
-%insertElementIntoListEnd(El, List, NewList)
+isElementInList(El,List):- ([El|_] = List);([_|T] = List,isElementInList(El,T)).
 
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists([], L, L) .
+mergeLists([H|T], L1, [H|L2]) :- mergeLists(T, L1, L2).
+%Definitions for reverseList(List, ReversedList)
+reverseHelper([],L,L).
+reverseHelper([H|T],L1,L2):- reverseHelper(T,L1,[H|L2]).
+reverseList(List,ReversedList):- reverseHelper(List,ReversedList,[]).
+%insertElementIntoListEnd(El, List, NewList)
+insertElementIntoListEnd(El, List, NewList):- mergeLists(List,[El],NewList).
